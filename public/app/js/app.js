764a0ce3797cec9c6a1c57f847113d39e1eb348b
John Santiago <jdsantiagojr@gmail.com>
2013-07-12 17:38:09 -0400

    Added controller test.

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('ckApp', ['ckControllers']).
-  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+angular.module('myApp', ['myApp.controllers']).
+  config(['$routeProvider', function($routeProvider) {
     $routeProvider.
       when('/', {    
 		templateUrl: 'partials/hello',
-        controller: 'MainController'
+        controller: 'MyCtrl2'
       });
     $locationProvider.html5Mode(true);
 }]);
---
+++ public/app/js/controllers/mainController.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/* Controllers */
+angular.module('myApp.controllers', [])
+    .controller('MyCtrl2', ['$scope', function($scope) {
+
+    }]);
