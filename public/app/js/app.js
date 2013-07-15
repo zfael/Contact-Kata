e4c3b4db01aca33af5832b4050b6029c7da7ffc7
Diego Escalante <descalante@commandalkon.com>
2013-07-15 12:00:54 -0500

    Added initial views and css styles. Changed myApp back to ckApp

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,11 +1,16 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.controllers']).
-  config(['$routeProvider', function($routeProvider) {
+angular.module('ckApp', ['ckControllers']).
+  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
       when('/', {    
 		templateUrl: 'partials/hello',
+        controller: 'MyCtrl2'
+      })
+
+      .when('/edit', {
+        templateUrl: 'partials/edit',
         controller: 'MyCtrl2'
       });
     $locationProvider.html5Mode(true);
--- public/app/js/controllers/mainController.js
+++ public/app/js/controllers/mainController.js
@@ -1,7 +1,9 @@
 'use strict';
 
 /* Controllers */
-angular.module('myApp.controllers', [])
+angular.module('ckControllers', [])
     .controller('MyCtrl2', ['$scope', function($scope) {
-
+        //Testing variables. These can be removed.
+        $scope.message = 'Hello World';
+        $scope.contacts = [{first: 'John', last: 'Doe', email: 'jdoe@email.com'}, {first: 'Nancy', last: 'Coolperson', email: 'ncoolperson@email.com'}, {first: 'Michael', last: 'Bluth', email: 'mbluth@email.com'}]
     }]);
