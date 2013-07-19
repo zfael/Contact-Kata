c9373f5a0dd570f30c2e10f1f82d4d1c45b1b142
Diego Escalante <descalante@commandalkon.com>
2013-07-19 17:03:49 -0500

    Improvement to partials and css.

--- public/app/js/controllers/mainController.js
+++ public/app/js/controllers/mainController.js
@@ -3,7 +3,6 @@
 /* Controllers */
 angular.module('ckControllers', [])
     .controller('MyCtrl2', ['$scope', function($scope) {
-        //Testing variables. These can be removed.
-        $scope.message = 'Hello World';
+        //Testing variable. This can be removed.
         $scope.contacts = [{first: 'John', last: 'Doe', email: 'jdoe@email.com'}, {first: 'Nancy', last: 'Coolperson', email: 'ncoolperson@email.com'}, {first: 'Michael', last: 'Bluth', email: 'mbluth@email.com'}]
     }]);
