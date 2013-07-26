e10965e9ae702dba2b6fa6e0056b141a9348b4fa
ssmith2762 <ssmith@commandalkon.com>
2013-07-26 17:00:50 -0400

    added addl tests

--- test/e2e/53320143-list-contacts.js
+++ test/e2e/53320143-list-contacts.js
@@ -4,45 +4,97 @@ var fs = require('fs');
 var util = require('util');
 var protractor = require('../../node_modules/protractor/lib/protractor.js');
 var ptor = protractor.getInstance();
-var mockServices = fs.readFileSync('test/mocks/mockServices.js').toString();
+var timeout = 50000;
 
-describe('Kata-Contact', function() {
+describe('Contact-Kata', function() {
 
     beforeEach(function() {
 
-        ptor.get('/');
-         //ptor.addMockModule('cai.services', mockServices);
+        ptor.get('http://localhost:3000');
+        //ptor.waitForAngular();
 
-     }, 100000);
+     }, 10000);
+
+
+    it('should display a greeting', function() {
+
+        ptor.findElement(protractor.By.xpath('/html/body/div/p'))
+            .getText().then(function(text) {
+                expect(text).toEqual('hi! Contacts will fill up here when you have them.');
+            });
+    }, timeout);
 
 
     it('should display the current contacts', function() {
 
-        ptor.findElement(protractor.By.xpath('/html/body/div'))
+        ptor.findElement(protractor.By.xpath('/html/body/div/ul'))
             .getAttribute('childElementCount').then(function(count) {
-                expect(count).toEqual(5);
+                expect(count).toEqual('3');
         });
 
-    }, 10000);
+    }, timeout);
 
 
-    it('should display Name for the first contact', function() {
+    it('should display Name, Contact informaiton the first contact', function() {
+        
+        //The following produces an error as documented to be caused by css selectors in markup
+        //InvalidElementStateError: invalid element state: SyntaxError: DOM Exception 12
+        // ptor.findElement(protractor.By.repeater('person in contacts').row(1))
+        //     .findElement(protractor.By.css('li:nth-child[1]'))
+        //         .getText().then(function(text) {
+        //             expect(text).toEqual('Bluth, Mike');
+        //         });
 
-        var contact = ptor.findElement(protractor.By.id('contact_1'));
-        contact.findElement(protractor.By.id('name_1')).
-          getText().then(function(text) {
-            expect(text).toContain('Doe, Jane');
-        });
-    }, 10000);
+        ptor.findElement(protractor.By.xpath('/html/body/div/ul/li[1]/ul/li[1]'))
+            .getText().then(function(text) {
+                expect(text).toEqual('Bluth, Michael');
+            });
+
+        ptor.findElement(protractor.By.xpath('/html/body/div/ul/li[1]/ul/li[2]'))
+            .getText().then(function(text) {
+                expect(text).toEqual('mbluth@email.com');
+            });
+
+    }, timeout);
 
     
-    it('should display Email Address for the first contact', function() {
+    it('should display Name, Contact informaiton the second contact', function() {
+        
+        ptor.findElement(protractor.By.xpath('/html/body/div/ul/li[2]/ul/li[1]'))
+            .getText().then(function(text) {
+                expect(text).toEqual('Coolperson, Nancy');
+            });
 
-        var contact = ptor.findElement(protractor.By.id('contact_1'));
-        contact.findElement(protractor.By.id('email_1')).
-          getText().then(function(text) {
-            expect(text).toContain('jdoe@gmail.com');
-        });
-    }, 10000);
+        ptor.findElement(protractor.By.xpath('/html/body/div/ul/li[2]/ul/li[2]'))
+            .getText().then(function(text) {
+                expect(text).toEqual('ncoolperson@email.com');
+            });
+
+    }, timeout);
+
+
+    it('should display Name, Contact informaiton the third contact', function() {
+        
+        ptor.findElement(protractor.By.xpath('/html/body/div/ul/li[3]/ul/li[1]'))
+            .getText().then(function(text) {
+                expect(text).toEqual('Doe, John');
+            });
+
+        ptor.findElement(protractor.By.xpath('/html/body/div/ul/li[3]/ul/li[2]'))
+            .getText().then(function(text) {
+                expect(text).toEqual('jdoe@email.com');
+            });
+
+    }, timeout);
+
+
+    it('should present an Add Button', function() {
+
+        ptor.findElement(protractor.By.className('icon-plus'))
+            .getText().then(function(text) {
+                expect(text).toEqual('');
+            });
+
+    }, timeout);
 
 });
