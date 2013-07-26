578baf6e9af7cb5bda53195a1011a91e11f2fd84
ssmith2762 <ssmith@commandalkon.com>
2013-07-26 17:00:24 -0400

    minor update for protractor version

--- config/protractorConf.js
+++ config/protractorConf.js
@@ -2,9 +2,11 @@
 exports.config = {
   // The address of a running selenium server. If this is specified,
   // seleniumServerJar and seleniumPort will be ignored.
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
   
-  //seleniumServerJar: 'test/selenium/selenium-server-standalone-2.33.0.jar',
+  seleniumServerJar: 'test/selenium/selenium-server-standalone-2.33.0.jar',
+  chromeDriver: './test/selenium/chromedriver',
+  seleniumArgs: [],
 
   // A base URL for your application under test. Calls to protractor.get()
   // with relative paths will be prepended with this.
@@ -15,11 +17,12 @@ exports.config = {
     'browserName': 'chrome'
   },  
 
+  specs: ['./test/e2e/53320143-list-contacts.js'],
   // Options to be passed to Jasmine-node.
+  
   jasmineNodeOpts: {
     // Spec folders are relative to the current working directly when
     // protractor is called. This can be folders or files.
-    specFolders: ['./test/e2e/53320143-list-contacts.js'],
     isVerbose: false,
     showColors: true,
     includeStackTrace: false,
