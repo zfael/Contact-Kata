354b1615ba2b350bfa4a1638e3a0af3a2b78a160
John Santiago Jr <jdsantiagojr@gmail.com>
2013-07-26 16:57:01 -0400

    Added edit and save controllers.

--- public/app/js/controllers/mainController.js
+++ public/app/js/controllers/mainController.js
@@ -2,17 +2,49 @@
 
 /* Controllers */
 angular.module('ckControllers', [])
-    .controller('MyCtrl2', ['$scope', function($scope) {
-<<<<<<< .merge_file_a05700
+    .controller('MyCtrl1', ['$scope', '$rootScope', '$location', function($scope, $rootScope, $location) {
         //Testing variables. These can be removed.
         $scope.message = 'hi';
-        $scope.contacts = [
-            {first: 'John', last: 'Doe', email: 'jdoe@email.com'},
-            {first: 'Nancy', last: 'Coolperson', email: 'ncoolperson@email.com'},
-            {first: 'Michael', last: 'Bluth', email: 'mbluth@email.com'}
-        ];
-=======
-        //Testing variable. This can be removed.
-        $scope.contacts = [{first: 'John', last: 'Doe', email: 'jdoe@email.com'}, {first: 'Nancy', last: 'Coolperson', email: 'ncoolperson@email.com'}, {first: 'Michael', last: 'Bluth', email: 'mbluth@email.com'}]
->>>>>>> .merge_file_a01756
+
+        $scope.edit = function (uid) {
+            for (var i = 0, n = $rootScope.contacts.length; i < n; ++i) {
+                if ($rootScope.contacts[i].id == uid) {
+                    // get information
+                    $location.path('/edit' + i);
+                    break;
+                }
+            }
+        };
+    }])
+    .controller('MyCtrl2', ['$scope','$rootScope','$routeParams', function ($scope, $rootScope, $routeParams) {
+        var id = null;
+        $scope.first = null;
+        $scope.last = null;
+        $scope.email = null;
+        $scope.editing = false;
+
+        if ($routeParams.id) {
+            id = $routeParams.id;
+
+            $scope.first = $rootScope.contacts[id].first;
+            $scope.last = $rootScope.contacts[id].last;
+            $scope.email = $rootScope.contacts[id].email;
+        }
+
+        $scope.save = function () {
+            if (!id) {
+                $rootScope.contacts.push({
+                    id: $rootScope.contacts.length + 1,
+                    first: $scope.first,
+                    last: $scope.last,
+                    email: $scope.email
+                });
+            }
+            else {
+                $rootScope.contacts[id].first = $scope.first;
+                $rootScope.contacts[id].last  = $scope.last;
+                $rootScope.contacts[id].email = $scope.email;
+            }
+        }
     }]);
+
